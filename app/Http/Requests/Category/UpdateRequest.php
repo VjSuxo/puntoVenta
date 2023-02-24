decc3f272e0e4274183bc415c6d9e1d859896885
kriakemor <vjoelsuxod@gmail.com>
2023-02-24 12:48:52 -0400

    base

---
+++ app/Http/Requests/Category/UpdateRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'=>'required|string|max:50',
+            'description'=>'nullable|string|max:250',
+        ];
+    }
+    public function messages(){
+        return [
+            'name.required'=>'Este campo es requerido',
+            'name.string'=>'El valor no es correcto',
+            'name.max'=>'Solo se permiten 50 caracteres',
+            'desciption.string'=>'el valor no es correcto',
+            'discription.max'=>'solo se permiten 250 caracteres',
+        ];
+    }
+}
